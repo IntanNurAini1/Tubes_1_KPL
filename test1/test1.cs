ae95d6a865813b7a8a4c83aa8aff81eb0011af7d
IntanNurAini <intan.nuraini2005@gmail.com>
2025-05-04 08:42:46 +0700

    perbaikan test

--- test1/test1.cs
+++ test1/test1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tubes_1_KPL.Model;
 using Tubes_1_KPL.Controller;
@@ -11,36 +12,42 @@ namespace test1
         [TestMethod]
         public async Task Login_Berhasil()
         {
-            var mockController = new MockLoginRegisterController(true);
+            var mockController = new MockLoginRegisterController();
+            mockController.AddUser("user", "user123"); 
             var automata = new LoginRegisterAutomata(mockController);
-            await automata.Login("user", "pass");
+
+            await automata.Login("user", "user123");
+
             Assert.AreEqual(LoginRegisterAutomata.State.LoggedIn, automata.CurrentState);
         }
 
         [TestMethod]
         public async Task Login_Gagal()
         {
-            var mockController = new MockLoginRegisterController(false);
+            var mockController = new MockLoginRegisterController();
+            mockController.AddUser("user", "user123"); 
             var automata = new LoginRegisterAutomata(mockController);
-            await automata.Login("user", "passe");
+
+            await automata.Login("user", "wrongpassword");
+
             Assert.AreEqual(LoginRegisterAutomata.State.LoggedOut, automata.CurrentState);
         }
 
-
+        
         private class MockLoginRegisterController : LoginRegisterController
         {
-            private readonly bool _loginResult;
+            private readonly Dictionary<string, string> _users = new();
 
-            public MockLoginRegisterController(bool loginResult)
+            public void AddUser(string username, string password)
             {
-                _loginResult = loginResult;
+                _users[username] = password;
             }
 
             public override Task<bool> TryLoginAsync(string username, string password)
             {
-                return Task.FromResult(_loginResult);
+                bool success = _users.ContainsKey(username) && _users[username] == password;
+                return Task.FromResult(success);
             }
-
 
             public override Task LogoutAsync(string username)
             {
@@ -54,6 +61,7 @@ namespace test1
 
             public override Task RegisterAsync(string username, string password)
             {
+                AddUser(username, password);
                 return Task.CompletedTask;
             }
         }
